e987463e064b37295744fb07918a8cbec486df0d
Juan Manuel <juanmanueldiaz85@gmail.com>
2021-05-08 23:13:06 -0300

    Added next-auth configuration file

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,57 @@
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export default NextAuth({
+  // https://next-auth.js.org/configuration/providers
+  providers: [
+    Providers.Credentials({
+      name: 'Credentials',
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: {  label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        const user = { id: 1, name: 'Jones Ferdinand', email: 'jferdinand@example.com', image: 'https://randomuser.me/api/portraits/men/3.jpg' }
+  
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null or false then the credentials will be rejected
+          return null
+        }
+      }
+    })
+  ],
+  secret: process.env.SECRET,
+  session: {
+    jwt: true,
+    maxAge: 5 * 60, // 5 minutes
+  },
+  pages: {
+    // signIn: '/auth/signin',  // Displays signin buttons
+    // signOut: '/auth/signout', // Displays form with sign out button
+    // error: '/auth/error', // Error code passed in query string as ?error=
+    // verifyRequest: '/auth/verify-request', // Used for check email page
+    // newUser: null // If set, new users will be directed here on first sign in
+  },
+
+  // Callbacks are asynchronous functions you can use to control what happens
+  // when an action is performed.
+  // https://next-auth.js.org/configuration/callbacks
+  callbacks: {
+    // async signIn(user, account, profile) { return true },
+    // async redirect(url, baseUrl) { return baseUrl },
+    // async session(session, user) { return session },
+    // async jwt(token, user, account, profile, isNewUser) { return token }
+  },
+
+  // Events are useful for logging
+  // https://next-auth.js.org/configuration/events
+  events: {},
+
+  // Enable debug messages in the console if you are having problems
+  debug: false,
+})
